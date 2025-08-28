5b3d198851c07e8fcee1b1db3951686e6cc9d92c
minhhien <hienminh.ce@gmail.com>
2025-08-29 00:08:23 +0700

    Lab1_ex4

---
+++ MCU_Excercise/Core/Inc/led_7seg.h
@@ -0,0 +1,17 @@
+/*
+ * led_7seg.h
+ *
+ *  Created on: Aug 28, 2025
+ *      Author: qwein
+ */
+
+#ifndef INC_LED_7SEG_H_
+#define INC_LED_7SEG_H_
+
+#include "main.h"
+
+
+
+void display7SEG(int num);
+
+#endif /* INC_LED_7SEG_H_ */
